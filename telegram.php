9352a98a71295199f303310ce98a65dabf75894d
Johaz1 <77331390+Johaz1@users.noreply.github.com>
2021-01-12 14:01:20 +0200

    telegram.php
    <?php
    
    /* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
    где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
    
    $name = $_POST['name'];
    $phone = $_POST['phone'];
    $email = $_POST['email'];
    $msg = $_POST['msg'];
    $token = "1599177496:AAFj5uxrMOXX15fnj1WntpHC-yx9xFaxNKA";
    $chat_id = "-1001333573144";
    $arr = array(
      'Имя пользователя: ' => $name,
      'Телефон: ' => $phone,
      'Email' => $email,
      'Сообщение:' => $msg
    );
    
    foreach($arr as $key => $value) {
      $txt .= "<b>".$key."</b> ".$value."%0A";
    };
    
    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
    
    $sendToTelegram2 = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
    
    if ($sendToTelegram && $sendToTelegram2) {
      header('Location: Thanks.html');
    } else {
      echo "Error";
    }
    ?>

---
+++ telegram.php
@@ -0,0 +1,32 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+$token = "1599177496:AAFj5uxrMOXX15fnj1WntpHC-yx9xFaxNKA";
+$chat_id = "-1001333573144";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+  'Сообщение:' => $msg
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+$sendToTelegram2 = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: Thanks.html');
+} else {
+  echo "Error";
+}
+?>
